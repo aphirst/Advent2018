94d491ba580f953d7583a557ca96c9c2eff4a7b6
Adam Hirst <adam@aphirst.karoo.co.uk>
2018-12-04 13:36:23 +0000

    Implement "smarter" solution to the second Day 01 problem
    This approach defines a binary tree data structure, along with a method
    which reports duplicates while attempting to insert a new entry.
    
    This turns out to be much faster than the previous approaches.
    
    An even faster approach would be to use a kind of hash table.

--- src/day01.f90
+++ src/day01.f90
@@ -17,6 +17,15 @@
 module Day01
   implicit none
 
+  type NodeInteger
+    integer                        :: key
+    type(NodeInteger), allocatable :: left, right
+  contains
+    procedure :: Create => NodeIntegerCreate
+    procedure :: InsertUnique => NodeIntegerInsertUnique
+    procedure :: PrintTree => NodeIntegerPrintTree
+    procedure :: Destroy => NodeIntegerDestroy
+  end type
 
   contains
 
@@ -202,6 +211,99 @@ module Day01
       write(unit,*) totals(i)
     end do
     close(unit)
+  end subroutine
+
+  subroutine NodeIntegerCreate(this, key)
+    class(NodeInteger)             :: this
+    integer,            intent(in) :: key
+
+    call this%Destroy()
+    this%key = key
+  end subroutine
+
+  recursive subroutine NodeIntegerInsertUnique(this, key, is_duplicate)
+    ! node insertion with no dupes
+    class(NodeInteger), intent(in out) :: this
+    integer,            intent(in)     :: key
+    logical,            intent(out)    :: is_duplicate
+
+    is_duplicate = .false.
+    if (key == this%key) then
+      is_duplicate = .true.
+      return
+    else if (key < this%key) then
+      if (allocated(this%left)) then
+        call this%left%InsertUnique(key, is_duplicate)
+      else
+        allocate(this%left)
+        this%left%key = key
+      end if
+    else
+      if (allocated(this%right)) then
+        call this%right%InsertUnique(key, is_duplicate)
+      else
+        allocate(this%right)
+        this%right%key = key
+      end if
+    end if
+  end subroutine
+
+  recursive subroutine NodeIntegerPrintTree(this)
+    class(NodeInteger), intent(in) :: this
+
+    if (allocated(this%left)) call this%left%PrintTree()
+    print *, this%key
+    if (allocated(this%right)) call this%right%PrintTree()
+  end subroutine
+
+  recursive subroutine NodeIntegerDestroy(this)
+    class(NodeInteger) :: this
+
+    if (allocated(this%left)) then
+      call this%left%Destroy()
+      deallocate(this%left)
+    end if
+    if (allocated(this%right)) then
+      call this%right%Destroy()
+      deallocate(this%right)
+    end if
+    this%key = 0
+  end subroutine
+
+  subroutine Problem01b_smarter()
+    ! use a binary search tree
+    integer                        :: unit, iostat
+    integer                        :: nextint, total, i
+    integer,           allocatable :: diffs(:)
+    type(NodeInteger), allocatable :: mytree
+    logical                        :: is_duplicate
+
+    total = 0
+    allocate(diffs(0))
+    is_duplicate = .false.
+
+    open(newunit=unit, file="day01.txt", iostat=iostat, status="old")
+    if (iostat /= 0) stop "Datenfehler."
+    read(unit, *, iostat=iostat) nextint
+    do while (iostat == 0)
+      diffs = [ diffs, nextint ]
+      read(unit, *, iostat=iostat) nextint
+    end do
+    close(unit)
+
+    outer: do
+      inner: do i = 1, size(diffs)
+        total = total + diffs(i)
+        if (.not. allocated(mytree)) then
+          allocate(mytree)
+          call mytree%Create(total)
+        else
+          call mytree%InsertUnique(total, is_duplicate)
+          if (is_duplicate) exit outer
+        end if
+      end do inner
+    end do outer
+    print "(a,i0)", "Ergebnis: ", total
   end subroutine
 
 end module
